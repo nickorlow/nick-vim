92148143a22bc17a8264a594da6bfc8e37f808b4
Nicholas Orlowsky <nickorlow@nickorlow.com>
2023-12-18 13:12:03 -0500

    fix clipboard

--- nvim.sh
+++ nvim.sh
@@ -5,7 +5,7 @@ echo Starting container...
 cur_dir=`pwd`
 container_name=${cur_dir////$'_'}
 container_name="${container_name:1}_$RANDOM"
-docker run --name $container_name --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --mount type=bind,source="$(pwd)",target=/work -d nick-vim &> /dev/null
+docker run --name $container_name --network host -e DISPLAY=$DISPLAY  --privileged  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /tmp/.X11-unix:/tmp/.X11-unix --mount type=bind,source="$(pwd)",target=/work -d nick-vim &> /dev/null
 
 echo Execing into container...
 docker exec -w /work -it $container_name /nvim-linux64/bin/nvim 
